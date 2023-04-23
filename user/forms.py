e815d9fc97d42a302d45f12f385734ccdfe4f9f3
Sanat Tobyl <119811878+Serguha6667@users.noreply.github.com>
2023-04-24 01:03:21 +0600

    profile2

--- user/forms.py
+++ user/forms.py
@@ -1,6 +1,7 @@
 from django.forms import Form, ModelForm
 from django import forms
 from django.contrib.auth.models import User
+from user.models import Profile
 
 
 class LoginForm(Form):
@@ -38,3 +39,10 @@ class UserRegistrationForm(ModelForm):
         if commit:
             user.save()
         return user
+
+
+class ProfileForm(ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['bio', 'image']
+
